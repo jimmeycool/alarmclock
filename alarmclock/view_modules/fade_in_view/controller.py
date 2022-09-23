65189bb2cb171c1156b79e275c5fabcb3bcf5eba
Glenn Hanawalt <glennhanawalt@gmail.com>
2022-09-23 10:59:19 -0700

    improve fade in view

--- alarmclock/view_modules/fade_in_view/controller.py
+++ alarmclock/view_modules/fade_in_view/controller.py
@@ -1,9 +1,14 @@
+from dataclasses import dataclass
 from typing import Any
 import timeit
 from alarmclock.engine.components import ControllerBase
 
+@dataclass
+class Model:
+  percent_progress: float
+
 class Controller(ControllerBase):
-  fade_duration = 1
+  fade_duration = 3
 
   def __init__(self, config: Any = None) -> None:
     super().__init__(config)
@@ -12,9 +17,5 @@ class Controller(ControllerBase):
 
   def update(self) -> Any:
     elasped_time = timeit.default_timer() - self.start_time
-    if (elasped_time < self.fade_duration):
-      # interpolate from black to target color
-      ratio = elasped_time / self.fade_duration
-      return tuple([int(ratio * value) for value in self.color])
-
-    return self.color
+    percent_progress = min(elasped_time / self.fade_duration, 1)
+    return Model(percent_progress)
--- alarmclock/view_modules/fade_in_view/view.py
+++ alarmclock/view_modules/fade_in_view/view.py
@@ -1,11 +1,41 @@
 from alarmclock.engine.components import ViewBase
-from PIL import Image, ImageDraw
+from PIL import Image
 from typing import Any, Tuple
+import random
+import math
 
 class View(ViewBase):
 
+  def __init__(self, config: Any = None) -> None:
+    super().__init__(config)
+    self.image = None
+    self.pixels = None
+    self.total_pixels = None
+    self.filled_count = 0
+
+  def scale_percent_progress(self, percent_progress: float):
+    return percent_progress**2
+
+  def color_pixel_by_index(self, pixel_position: int):
+    y = pixel_position // self.dimensions[0]
+    x = pixel_position % self.dimensions[1]
+    self.image.putpixel((x, y), tuple(self.config["color"]))
+
   def draw(self, dimensions: Tuple[int, int], model: Any = None) -> Image:
-    image = Image.new("RGB", dimensions, (0, 0, 0))
-    draw = ImageDraw.Draw(image)
-    draw.rectangle([(0, 0), (128, 128)], fill=model)
-    return image
+    if self.image == None:
+      self.dimensions = dimensions # todo, move to constructor
+      self.image = Image.new("RGB", dimensions, (0, 0, 0))
+      self.total_pixels = dimensions[0] * dimensions[1]
+      self.pixel_sequence = list(range(0, self.total_pixels))
+      random.shuffle(self.pixel_sequence)
+
+    total_fill_percentage = self.scale_percent_progress(model.percent_progress)
+    total_fill_count = math.ceil(total_fill_percentage * self.total_pixels)
+    iteration_fill_count = total_fill_count - self.filled_count
+
+    for _ in range(0, iteration_fill_count):
+      pixel_position = self.pixel_sequence[self.filled_count]
+      self.color_pixel_by_index(pixel_position)
+      self.filled_count += 1
+
+    return self.image.copy()
